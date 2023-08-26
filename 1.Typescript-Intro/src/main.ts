3a535805254797738951d1f4c1d35d21e326616a
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-27 02:41:56 +0530

    typescript-intro

---
+++ 1.Typescript-Intro/src/main.ts
@@ -0,0 +1,12 @@
+let username = "jignesh nandlal sharma"
+console.log(username);
+
+let a: number = 12
+let b: number = 6
+// let b: string = "6"
+let c: number = 2
+
+console.log(a / b);
+
+console.log(c * b);
+
