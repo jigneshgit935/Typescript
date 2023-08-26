ee3ca00dd40890940ad92443bf48650cfdb14d56
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-27 03:03:11 +0530

    basic-types in typesript

---
+++ 2.Basic-Types/build/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+let myName;
+myName = "jinu";
+// myName =21
+let myNumber;
+myNumber = 21;
+// myNumber = "jinu"
+let isLoading;
+isLoading = true;
+// isLoading = 2
+let album;
+album = 3;
+album = "kohinoor";
+album = true;
+// const sum = (a: number, b: number) => {
+//     return a + b
+// }
+const sum = (a, b) => {
+    return a + b;
+};
+let card;
+card = 23;
+card = "jinu";
---
+++ 2.Basic-Types/src/main.ts
@@ -0,0 +1,47 @@
+import { tr } from "@faker-js/faker"
+
+let myName: string
+myName = "jinu"
+// myName =21
+
+
+let myNumber: number
+myNumber = 21
+// myNumber = "jinu"
+
+
+let isLoading: boolean
+isLoading = true
+// isLoading = 2
+
+
+let album: any
+album = 3
+album = "kohinoor"
+album = true
+
+
+// const sum = (a: number, b: number) => {
+//     return a + b
+// }
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+
+let card: string | number
+card = 23
+card = "jinu"
+card = true
+
+
+// union type
+let isActive: number | boolean
+isActive = true
+isActive = 23
+isActive = "j"
+
+
+let re = /\w+/g
+// let re: RegExp = /\w+/g
